26dedcc29865c0dccaa4ec427f505f5347ef9aee
tasu47 <vartikakumari801@gmail.com>
2022-04-28 17:55:08 +0530

    make the whatsapp web page

---
+++ js/web.js
@@ -0,0 +1,7 @@
+var start = document.getElementById("start")
+start.addEventListener('click',function(){
+    // start.style.color ='red'
+})
+document.getElementById("now").addEventListener('click',function(){
+    start.style.color ='red'
+})
